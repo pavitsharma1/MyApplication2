b3c89dd0ea962b67befd2b4e44ab61d0ff26b930
pavitsharma1 <pavitsharmagood@gmail.com>
2025-02-10 13:52:04 +0530

    barriers and bars in barriersconstraint.xml
    the barriers constraint file consists the code for the  assignment barriers and bars from 10th feb 2025

--- app/src/main/java/com/example/myapplication/constraintactivity.kt
+++ app/src/main/java/com/example/myapplication/constraintactivity.kt
@@ -1,12 +1,22 @@
 package com.example.myapplication
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class constraintactivity : AppCompatActivity() {
+      lateinit var txtblack: TextView
+      lateinit var txtred: TextView
+      lateinit var txtblue: TextView
+      lateinit var txtskin: TextView
+      lateinit var txtyellow: TextView
+      lateinit var txtorange: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +26,34 @@ class constraintactivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        txtblack=findViewById(R.id.blackbox)
+        txtred=findViewById(R.id.redbox)
+        txtskin=findViewById(R.id.skinbox)
+        txtblue=findViewById(R.id.bluebox)
+        txtyellow=findViewById(R.id.yellowbox)
+        txtorange=findViewById(R.id.orangebox)
+
+        txtblack.setOnClickListener {
+            var intent = Intent(this, blackbox::class.java)
+                intent.putExtra("value",0)
+            startActivity(intent)
+        }
+        txtred.setOnClickListener {
+            startActivity(Intent(this, Redbox::class.java))
+        }
+        txtskin.setOnClickListener {
+            startActivity(Intent(this, skinbox::class.java))
+        }
+        txtblue.setOnClickListener {
+            startActivity(Intent(this, Redbox::class.java))
+        }
+        txtyellow.setOnClickListener {
+            startActivity(Intent(this, blackbox::class.java))
+        }
+        txtorange.setOnClickListener {
+            startActivity(Intent(this, Redbox::class.java))
+        }
+
     }
 }
